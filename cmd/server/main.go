41206f87dac544680d4a9b18b3e23de9778a2481
rnikbond <rnikbond@gmail.com>
2022-12-08 15:47:41 +0300

    Инициализация проекта

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"time"
+
+	_ "google.golang.org/grpc"
 )
 
 func main() {
 
+	//_ = grpc.Server{}
+
 	fmt.Printf("Server started at: %s\n", time.Now().Format("02-01-2006 15:04:05"))
-	time.Sleep(5 * time.Second)
+	time.Sleep(1 * time.Second)
 	fmt.Printf("Server stopped at: %s\n", time.Now().Format("02-01-2006 15:04:05"))
 
 }
