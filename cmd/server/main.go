921bdef5488432907411168a4ddc9d65ffd7aeb8
rnikbond <rnikbond@gmail.com>
2022-12-08 16:06:55 +0300

    Добавлена конфигурация для сервера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"GophKeeper/internal/server"
 	"fmt"
+	"log"
 	"time"
-
-	_ "google.golang.org/grpc"
 )
 
 func main() {
 
-	//_ = grpc.Server{}
+	cfg := server.NewConfig()
+	if err := cfg.ParseArgs(); err != nil {
+		log.Fatalln(err.Error())
+	}
 
-	fmt.Printf("Server started at: %s\n", time.Now().Format("02-01-2006 15:04:05"))
+	fmt.Printf("Server started on %s at: %s\n", cfg.AddrGRPC, time.Now().Format("02-01-2006 15:04:05"))
 	time.Sleep(1 * time.Second)
 	fmt.Printf("Server stopped at: %s\n", time.Now().Format("02-01-2006 15:04:05"))
 
---
+++ internal/server/config.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	AddrGRPC string `env:"ADDRESS_RPC" json:"address_rpc"`
+}
+
+// NewConfig Конфигурация сервера
+func NewConfig() *Config {
+
+	return &Config{
+		AddrGRPC: ":3200",
+	}
+}
+
+// ParseArgs Разброр аргументов командной строки
+func (cfg *Config) ParseArgs() error {
+
+	addr := flag.String("ag", "", "string - address grpc gate")
+	flag.Parse()
+
+	if addr == nil || len(*addr) == 0 {
+		*addr = cfg.AddrGRPC
+	}
+
+	if err := isValidAddress(*addr); err != nil {
+		return err
+	}
+
+	cfg.AddrGRPC = *addr
+	return nil
+}
+
+// isValidAddress Проверка валидности адреса сервера
+func isValidAddress(addr string) error {
+
+	if addr == "" {
+		return fmt.Errorf("address can not be empty")
+	}
+
+	parsedAddr := strings.Split(addr, ":")
+	if len(parsedAddr) != 2 {
+		return fmt.Errorf("need address in format host:port")
+	}
+
+	if len(parsedAddr[0]) > 0 && parsedAddr[0] != "localhost" {
+		if ip := net.ParseIP(parsedAddr[0]); ip == nil {
+			return fmt.Errorf("incorrect host: " + parsedAddr[0])
+		}
+	}
+
+	if _, err := strconv.Atoi(parsedAddr[1]); err != nil {
+		return fmt.Errorf("incorrect port: " + parsedAddr[1])
+	}
+
+	return nil
+}
